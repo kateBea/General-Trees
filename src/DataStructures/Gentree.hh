af3ee964793baf969a9f1c13f3d95d41da63235d
ケイト <55351037+kateBea@users.noreply.github.com>
2024-12-17 18:08:25 +0100

    forall implementation

--- src/DataStructures/Gentree.hh
+++ src/DataStructures/Gentree.hh
@@ -14,7 +14,16 @@ namespace Mikoto {
         using reference_type = T;
 
         template<typename... Args>
-        auto Insert(Args&&... args) -> bool;
+        auto Insert(Args&&... args) -> bool {
+            auto result{ std::make_unique<Node>(std::forward<Args>(args)...) };
+
+            if (result) {
+                m_Nodes.emplace_back(std::move(result));
+                return true;
+            }
+
+            return false;
+        }
 
         template<typename UnaryPred, typename... Args>
         auto InsertChild(UnaryPred&& pred, auto&&... args) -> bool;
@@ -28,6 +37,21 @@ namespace Mikoto {
         template<typename UnaryPred>
         auto Get(UnaryPred&& pred) -> reference_type;
 
+        template<typename UnaryFunc>
+        auto ForAll(UnaryFunc&& func) -> void {
+
+            for (auto& node : m_Nodes) {
+                Traverse(func, *node);
+            }
+        }
+
+        template<typename UnaryFunc, typename UnaryPred>
+        auto ForAll(UnaryFunc&& func, UnaryPred&& pred) -> void {
+            for (auto& node : m_Nodes) {
+                TraverseWithPred(func, pred, *node);
+            }
+        }
+
     private:
         struct Node {
             template<typename... Args>
@@ -40,6 +64,27 @@ namespace Mikoto {
             value_type data;
             std::vector<std::unique_ptr<Node>> children;
         };
+
+        template<typename UnaryFunc>
+        auto Traverse(UnaryFunc&& func, Node& node) -> void {
+            func(node.data);
+
+            for (auto& child : node.children) {
+                Traverse(func, *child);
+            }
+        }
+
+        template<typename UnaryFunc, typename UnaryPred>
+        auto TraverseWithPred(UnaryFunc&& func, UnaryPred&& pred, Node& node) -> void {
+            if (pred(node.data)) {
+                func(node.data);
+            }
+
+            for (auto& child : node.children) {
+                Traverse(func, *child);
+            }
+        }
+
     
     private:
         std::vector<std::unique_ptr<Node>> m_Nodes{};
--- test/main.cc
+++ test/main.cc
@@ -2,8 +2,45 @@
 
 #include <DataStructures/Gentree.hh>
 
+class Entity {
+public:
+    explicit Entity(int id) noexcept
+        : m_Id{ id }
+    {
+        
+    }
+
+    auto GetId() const -> int {
+        return m_Id;
+    }
+
+
+private:
+    int m_Id{};
+};
+
 int main(int, char**) {
+    using namespace Mikoto;
+
     std::cout << "GenTree Test\n";
+
+    GenTree<Entity> entities{};
+
+    entities.Insert(-1);
+    entities.Insert(1);
+    entities.Insert(2);
+    entities.Insert(3);
+    entities.Insert(4);
+    entities.Insert(5);
+
+    std::cout << "Printing all entities:\n";
+    entities.ForAll([](auto& elem) { std::cout << "Root: " << elem.GetId() << std::endl; });
+
+    std::cout << "\nPrinting all entities with Id greater than 3:\n";
+    entities.ForAll([](auto& elem) { std::cout << "Root: " << elem.GetId() << std::endl; },
+                [](auto& elem) { return elem.GetId() > 3; });
+
+    std::cout << "Exiting program...\n";
 
     return 0;
 }
